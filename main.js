abf06a5b9f8425c3dc846df41a1eb1a809c5c203
warnerfleming <fleming14@usf.edu>
2024-10-20 23:28:10 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+//Task 2 and 3
+const pricing = document.getElementById("Product-price")
+const sizing = document.getElementById("Select-Size")
 
+
+const purchaseB = document.getElementById("purchase-button")
+
+function updatepurchaseB(){
+if (stock === 'out of stockj'){
+purchaseB
+
+}
+sizing.addEventListener('change', (event) =>)
+
+}
